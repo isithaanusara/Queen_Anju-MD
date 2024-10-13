a4138f78e0f950ddfa9d2b4065d2ac0b4145819a
isithaanusara <isithaanusara01@gmail.com>
2024-10-13 18:21:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,6 +21,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "add youe session id here",
-MONGODB: process.env.MONGODB || "your mongodb uri here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=qBUy1bCR#39h1qi3T-_n8wejPqU448-8ehxzXwMsS64z0itmhgBc",
+MONGODB: process.env.MONGODB || "mongodb+srv://isithaanusara01:202213isi@cluster0.mcee3.mongodb.net/",
 };
